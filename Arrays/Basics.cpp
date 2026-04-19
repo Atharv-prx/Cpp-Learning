93dd56f676090f93c1d09a55626c634bc722752e
Atharv-prx <atharvvrm6969@gmail.com>
2026-04-19 19:48:13 +0530

    Can declare an array 2 ways

---
+++ Arrays/Basics.cpp
@@ -0,0 +1,11 @@
+# include <iostream>
+int main(){
+
+    std::string Flavours[] = {"Vanilla", "Chocolate", "Strawberry", "Mango"};
+
+    std::cout << Flavours[0] << '\n';
+    std::cout << Flavours[1] << '\n';
+    std::cout << Flavours[2] << '\n';
+    std::cout << Flavours[3] << '\n';
+    return 0;
+}
