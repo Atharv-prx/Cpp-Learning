c69e4f5d9af0481bf4b809c6c542150c4f9de4c8
Atharv-prx <atharvvrm6969@gmail.com>
2026-04-22 13:28:22 +0530

    Next up null pointer

---
+++ Basics/Pointers.cpp
@@ -0,0 +1,22 @@
+// Pointer = It is a variable that stores the memory address of another variable. 
+//           We use pointers because sometimes it is easier to use address.
+// & = adress of operator
+// * = dereference operator
+
+#include <iostream>
+int main(){
+
+    std::string name = "Yash";
+    std::string pokemons[3] = {"Pikachu", "Bulbasaur", "Charmander"};
+
+    std::string *pName = &name;
+    std::string *pPokemons = pokemons; // array is already an adress so we don't need to use & operator
+
+    std::cout << pName << '\n'; // contains the memory address of name variable
+    std::cout << *pName << '\n'; // accessing the value at the address stored within pointer variable
+
+    std::cout << pPokemons << '\n'; // contains the memory address of the first element of the array
+    std::cout << *pPokemons[2] << '\n'; // accessing the value at the address stored within pointer variable
+
+    return 0;
+}
